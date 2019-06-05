4a8a459db2f1cf057c75ce8bfe1d83e57bc72eb1
Nicolas Steven Miller <nicolasmiller@gmail.com>
2019-06-05 00:42:31 -0500

    fix author string

--- conf.py
+++ conf.py
@@ -129,7 +129,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'ardour.tex', 'Ardour Documentation',
-     'nicolas', 'manual'),
+     'Various', 'manual'),
 ]
 
 latex_engine = 'xelatex'
